f1dd8972de932cba7260a3992d05c48fe59e4bc7
Sam <strongzensam@gmail.com>
2019-02-03 11:38:16 -0800

    trying to see if Mongo db  URI is showing up on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,15 +1,15 @@
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://<SamSchwartz>:<fartjokes1!>@ds155293.mlab.com:55293/node-todo-api'
-};
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://<SamSchwartz>:<fartjokes1!>@ds155293.mlab.com:55293/node-todo-api'
+// };
 
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
+// mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
 
-
+console.log("HEYO BRO", process.env.MONGODB_URI);
 // mongoose.connect( db.localhost || db.mlab)
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 
 module.exports = {mongoose}
