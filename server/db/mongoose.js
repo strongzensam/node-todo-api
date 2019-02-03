8c6f816071c134ef5b705124045f7f003e6fcc1e
Sam <strongzensam@gmail.com>
2019-02-03 11:01:37 -0800

    modified db file to check deployment on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,12 @@
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://<SamSchwartz>:<fartjokes1!>@ds155293.mlab.com:55293/node-todo-api'
+};
+
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect( db.localhost || db.mlab)
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 
 module.exports = {mongoose}
