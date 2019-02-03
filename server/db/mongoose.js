8efa5dea1408197bad238615b1b54b6a17315e5d
Sam <strongzensam@gmail.com>
2019-02-03 11:49:37 -0800

    trying another modification to mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,11 +5,37 @@
 
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-// mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
 
-console.log("HEYO BRO", process.env.MONGODB_URI);
-// mongoose.connect( db.localhost || db.mlab)
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
+mongoose.Promise = global.Promise;
+
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://<SamSchwartz>:<fartjokes1!>@ds155293.mlab.com:55293/node-todo-api'
+};
+
+mongoose.connect(db.localhost,
+    {
+        useMongoClient: true
+    }
+).then(
+    () => {},
+    err =>
+    {
+        mongoose.connect(db.mlab,
+            {
+                useMongoClient: true
+            }
+        );
+    }
+);
+
+
+
+// mongoose.Promise = global.Promise;
+// // mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
+//
+// console.log("HEYO BRO", process.env.MONGODB_URI);
+// // mongoose.connect( db.localhost || db.mlab)
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 
 module.exports = {mongoose}
