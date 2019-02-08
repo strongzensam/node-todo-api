748f28a8403c537798fbc88363427587c932d4d4
Sam <strongzensam@gmail.com>
2019-02-08 06:04:07 -0800

    test the delete todos by ID route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -97,3 +97,39 @@ describe('GET /todos/:id', () => {
     .end(done)
   })
 })
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[0]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+        Todo.findById(hexId).then((todo) => {
+          console.log(todo);
+          expect(todo).toBeFalsy();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+  it('should return a 404 if todo not found', (done) => {
+      var id = new ObjectID();
+      request(app)
+      .delete(`/todos/${id.toHexString()}`)
+      .expect(404)
+      .end(done)
+
+  })
+  it('should return 404 if id is invalid', (done) =>{
+    request(app)
+    .delete('/todos/123')
+    .expect(404)
+    .end(done)
+  })
+});
